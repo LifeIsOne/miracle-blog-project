76390315361b2008ec02c2decfb7fa7be5b32a7c
LifeIsOne <didtmdgh258@gmail.com>
2024-09-01 18:20:50 +0900

    [REFACTORING] HttpRequest에서 Model로 변경

--- src/main/java/miracleblog/domain/_core/error/MiracleExceptionHandler.java
+++ src/main/java/miracleblog/domain/_core/error/MiracleExceptionHandler.java
@@ -3,6 +3,7 @@ package miracleblog.domain._core.error;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import miracleblog.domain._core.error.exception.Exception404;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class MiracleExceptionHandler {
 
-    @ExceptionHandler(Exception404.class)
-    public String ex404(Exception404 e, HttpServletRequest request) {
-        request.setAttribute("msg", e.getMessage());
-        request.setAttribute("errorCode", 404);
-        log.info("404 : " + e.getMessage());
-        return "err/err";
-    }
+@ExceptionHandler(Exception404.class)
+public String ex404(Exception404 e, Model model) {
+    model.addAttribute("msg", e.getMessage());
+    model.addAttribute("errorCode", 404);
+    log.info("404 : " + e.getMessage());
+    return "err/err";
+}
 }
